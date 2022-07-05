52e80b87871250c40d8b7a44d02735110eef7d44
Maxime <mcluzel@edenschool.fr>
2022-07-05 10:54:14 +0200

    Introduce sass

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import '../css/style.css';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+
+//BEM: Découper code en bloc
+//B : Bloc, E : Element, M : Modifier
+
+//Mix in: variable qu'on décalre et quand on l'appelle on met ce qu'on veut dedans
